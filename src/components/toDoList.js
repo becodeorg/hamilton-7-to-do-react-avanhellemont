aaf96724480b4406e22e95c4a445c620039c3ab9
Avanhellemont <vanhellemont.aurelie@gmail.com>
2022-11-21 12:31:10 +0100

    correction checkbox

--- src/components/toDoList.js
+++ src/components/toDoList.js
@@ -5,6 +5,11 @@ function Checkbox() {
   const handleClick = (event) => {
     // 👇️ toggle isActive state on click
     setIsActive((current) => !current);
+
+    if (event.target.checked) {
+      //si la cible de l'évenement est check
+      event.target.closest("li").style.textDecoration = "line-through"; //closest = proche
+    } else event.target.closest("li").style.textDecoration = "none";
   };
 
   return (
@@ -18,13 +23,17 @@ function Checkbox() {
 
 function ToDoList() {
   const initialTodos = ["Read a book", "Develop my projects"];
-  const [todos/*, setTodos*/] = useState(initialTodos);
+  const [todos /*, setTodos*/] = useState(initialTodos);
+
+  //event.target.closest('li').rajoute ce que tu veux içi
+
   return (
     <div className="ToDoList">
       <ul>
         {todos.map((todo) => (
           <li>
-            <Checkbox />{todo}
+            <Checkbox />
+            {todo}
           </li>
         ))}
       </ul>
