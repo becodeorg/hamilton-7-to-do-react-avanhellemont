8ecfa357d3c4597e4fd0fbad82a35d75f3c777f6
Avanhellemont <vanhellemont.aurelie@gmail.com>
2022-11-22 14:12:48 +0100

    interactive rendering

--- src/App.js
+++ src/App.js
@@ -1,22 +1,20 @@
 import React from "react";
 import Banner from "./components/banner";
 import AddToDo from "./components/addToDo";
-import ToDoList from "./components/toDoList";
 import "./sass/banner.scss";
 import "./sass/addToDo.scss";
 import "./sass/toDoList.scss";
 
 function App() {
   return (
-    <body className="ToDoApp">
+    <div className="ToDoApp">
       <header className="ToDoApp__header">
         <Banner />
       </header>
       <main className="ToDoApp__section">
         <AddToDo />
-        <ToDoList />
       </main>
-    </body>
+    </div>
   );
 }
 
--- src/components/addToDo.js
+++ src/components/addToDo.js
@@ -1,15 +1,63 @@
-import React from "react";
-function AddToDo() {
+import React, { useRef, useState } from "react";
+
+function Checkbox() {
+  const [isActive, setIsActive] = useState(false);
+
+  const handleClick = (event) => {
+    // 👇️ toggle isActive state on click
+    setIsActive((current) => !current);
+
+    if (event.target.checked) {
+      //si la cible de l'évenement est check
+      event.target.closest("li").style.textDecoration = "line-through"; //closest = proche
+    } else event.target.closest("li").style.textDecoration = "none";
+  };
+
   return (
-    <div className="AddToDo">
-      <input
+    <input
+      type="checkbox"
+      className={isActive ? "checked" : "not-checked"}
+      onClick={handleClick}
+    ></input>
+  );
+}
+export default function AddToDo() {
+  const initialTodos = ["Read a book", "Develop my projects"]
+  const [todos, setTodos] = useState(initialTodos);
+  const inputRef = useRef();
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const newTodo = [...todos];
+
+    const task = inputRef.current.value;
+    newTodo.push(task);
+    setTodos(newTodo);
+  };
+  
+  //affichage
+  return (
+<>
+    <form className="AddToDo" action="submit" onSubmit={handleSubmit}>
+      <input ref={inputRef}
         className="AddToDo__input"
         type="text"
         placeholder="Type a new todo"
       ></input>
-      <button className="AddToDo__button">Add Todo</button>
-      <hr />
-    </div>
+      <button className="AddToDo__button" >
+        Add Todo
+      </button>
+    </form>
+
+     <div className="ToDoList">
+     <ul>
+       {todos.map((todo, index) => (
+         <li key={index}>
+           <Checkbox />
+           {todo}
+         </li>
+       ))}
+     </ul>
+   </div>
+   </>
   );
-}
-export default AddToDo;
+};
